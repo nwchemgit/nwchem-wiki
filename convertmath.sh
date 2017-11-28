8e06468e73052665acb1c177d23a07615bbbd5d6
edoapra <edoardo.apra@gmail.com>
2017-11-28 09:44:17 -0800

    comment

--- convertmath.sh
+++ convertmath.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -f
+# requires readme2tex modified code from
+# https://github.com/edoapra/readme2tex.git
 #python -m readme2tex --username nwchemgit --project nwchem --wiki --output ZMATRIX----Z-matrix-input.md  ZMATRIX----Z-matrix-input_in.md
 cp $1.md $1_tmp.md
 sed -i 's/\\(/\$/g' $1_tmp.md
