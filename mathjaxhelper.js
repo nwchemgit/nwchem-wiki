3d9187dc8da2c77e8206ed641721054fa8fea105
edoapra <edoardo.apra@gmail.com>
2020-08-13 16:55:12 -0700

    extra config for mathjax

---
+++ mathjaxhelper.js
@@ -0,0 +1,9 @@
+MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX","output/CommonHTML"]
+  "HTML-CSS": {
+    availableFonts: [],
+    preferredFont: null,
+    webFont: "Latin-Modern"
+  }
+});
