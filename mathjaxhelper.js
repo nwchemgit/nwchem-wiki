6db9258e5c5f90c652e7409da0da2808adf930bd
edoapra <edoardo.apra@gmail.com>
2020-08-25 20:11:50 -0700

    rm

--- mathjaxhelper.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX","output/CommonHTML"]
-  "HTML-CSS": {
-    availableFonts: [],
-    preferredFont: null,
-    webFont: "Latin-Modern"
-  }
-});
