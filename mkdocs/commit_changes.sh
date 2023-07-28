42e73fd78e2518edf446aef664f21cea8926d29a
edoapra <edoardo.apra@gmail.com>
2023-07-28 15:17:39 -0700

    timezone

--- mkdocs/commit_changes.sh
+++ mkdocs/commit_changes.sh
@@ -3,6 +3,7 @@ git config user.name "nwchemgit"
 git config user.email "nwchemgit@gmail.com"
 git config advice.updateSparsePath false
 #git add -A
+TZ='America/Los_Angeles'; export TZ
 if git add .
 then
     git commit -m website\ changes\ on\ `date +%a_%b_%d_%H:%M:%S_%Y`
